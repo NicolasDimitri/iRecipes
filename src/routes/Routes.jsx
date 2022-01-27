0ce80429a53db1e5865dcce8a8ba7890eb1063ac
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-27 20:16:08 -0300

    :rotating_light: Fix: Adds The Login Page Component in all routes
    - Adds the component in the Route only to stop errors - When other components are created, simply swap the rendered component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path='/' component={}/>
-      <Route exact path='/foods' component={}/>
-      <Route exact path='/drinks' component={}/>
-      <Route exact path='/foods:id' component={}/>
-      <Route exact path='/drinks:id' component={}/>
-      <Route exact path='/foods:id/in-progress' component={}/>
-      <Route exact path='/explore' component={}/>
-      <Route exact path='/explore/foods' component={}/>
-      <Route exact path='/explore/drinks' component={}/>
-      <Route exact path='/explore/foods/ingredients' component={}/>
-      <Route exact path='/explore/drinks/ingredients' component={}/>
-      <Route exact path='/explore/foods/nationalities' component={}/>
-      <Route exact path='/profile' component={}/>
-      <Route exact path='/done-recipes' component={}/>
-      <Route exact path='/favorite-recipes' component={}/>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/foods" component={ Login } />
+      <Route exact path="/drinks" component={ Login } />
+      <Route exact path="/foods:id" component={ Login } />
+      <Route exact path="/drinks:id" component={ Login } />
+      <Route exact path="/foods:id/in-progress" component={ Login } />
+      <Route exact path="/explore" component={ Login } />
+      <Route exact path="/explore/foods" component={ Login } />
+      <Route exact path="/explore/drinks" component={ Login } />
+      <Route exact path="/explore/foods/ingredients" component={ Login } />
+      <Route exact path="/explore/drinks/ingredients" component={ Login } />
+      <Route exact path="/explore/foods/nationalities" component={ Login } />
+      <Route exact path="/profile" component={ Login } />
+      <Route exact path="/done-recipes" component={ Login } />
+      <Route exact path="/favorite-recipes" component={ Login } />
     </Switch>
   );
 }
