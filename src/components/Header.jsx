768a6d9fe9051fab75bfaeeed0e2b5f47f63c831
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-01-28 20:40:34 -0300

    Styling component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import AplicationContext from '../context/AplicationContext';
+import '../styles/Header.css';
 
 function Header({ title, renderExplore }) {
   const { toggleSearchBar } = useContext(AplicationContext);
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <nav className="nav">
         <Link to="/profile">
           <img
             data-testid="profile-top-btn"
@@ -17,10 +18,10 @@ function Header({ title, renderExplore }) {
             alt=""
           />
         </Link>
-        <h1 data-testid="page-title">{ title }</h1>
+        <h1 data-testid="page-title" className="title">{ title }</h1>
         {
           renderExplore && (
-            <button type="button" onClick={ toggleSearchBar }>
+            <button type="button" className="searchBtn" onClick={ toggleSearchBar }>
               <img
                 data-testid="search-top-btn"
                 src={ searchIcon }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import AplicationContext from '../context/AplicationContext';
+import '../styles/SearchBar.css';
 
 export default function SearchBar() {
   const { renderButton } = useContext(AplicationContext);
   if (!renderButton) return '';
   return (
-    <div>
+    <div className="divSearchInput">
       <input
         type="text"
         name="search"
         data-testid="search-input"
+        className="searchInput"
       />
     </div>
   );
