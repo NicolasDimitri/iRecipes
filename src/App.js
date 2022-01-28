3bbc2f903cf1016edcdd7545e1f9e534a848815c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-01-28 15:23:23 -0300

    Feat: Req5 Implementing email and password validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glasss
-      </object>
-    </div>
+    // <div className="meals">
+    //   <span className="logo">TRYBE</span>
+    //   <object
+    //     className="rocksGlass"
+    //     type="image/svg+xml"
+    //     data={ rockGlass }
+    //   >
+    //     Glasss
+    //   </object>
+    // </div>
+    <Login />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const validateForm = () => {
+    const minPasswordLength = 6;
+    const regex = /^[\w]+([.|\-|_][A-Za-z0-9]+)*@[a-z]{2,}(\.[a-z]{2,})+$/g;
+    const emailIsValid = regex.test(email);
+
+    if (password.length > minPasswordLength && emailIsValid) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  const handleSubmit = () => {
+    validateForm();
+  };
+
+  useEffect(() => {
+    validateForm();
+  }, [email, password]);
+
   return (
     <div className="container">
       <div className="w-auto p-3">
         <h1>Login</h1>
         <form className="form">
-          <input type="email" data-testid="email-input" name="email" />
-          <input type="password" data-testid="password-input" name="password" />
-          <button type="button" data-testid="login-submit-btn">Enter</button>
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value) }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-btn"
+            onClick={ handleSubmit }
+            disabled={ disabled }
+          >
+            Enter
+          </button>
         </form>
       </div>
     </div>
