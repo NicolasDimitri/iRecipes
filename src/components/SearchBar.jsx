8cea731c9bbbc5fba254219679b7a11ba47a246c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-01-28 21:05:58 -0300

    Feat:  Implementing search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,12 +7,36 @@ export default function SearchBar() {
   if (!renderButton) return '';
   return (
     <div className="divSearchInput">
-      <input
-        type="text"
-        name="search"
-        data-testid="search-input"
-        className="searchInput"
-      />
+      <form>
+        <input
+          type="text"
+          name="search"
+          data-testid="search-input"
+          className="searchInput"
+        />
+
+        <div className="radio-group">
+          <label htmlFor="optradio" className="radio-inline">
+            <input type="radio" name="optradio" data-testid="ingredient-search-radio" />
+            {' '}
+            Ingredient
+          </label>
+          <label htmlFor="optradio" className="radio-inline">
+            <input type="radio" name="optradio" data-testid="name-search-radio" />
+            {' '}
+            Name
+          </label>
+          <label htmlFor="optradio" className="radio-inline">
+            <input type="radio" name="optradio" data-testid="first-letter-search-radio" />
+            {' '}
+            First Letter
+          </label>
+        </div>
+
+        <button type="button" data-testid="exec-search-btn" className="search-btn">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
