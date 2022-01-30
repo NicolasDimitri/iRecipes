67b74899781862698c989ad02ed5480997d4874b
Savio Silva <s.drksilva@gmail.com>
2022-01-30 17:45:29 -0300

    Feat: built the footer component design;

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={ `primary_color box ${styles.box}` }>
+      <nav className="wrapper flex justify_content_between ">
+        <Link to="/profile" className={ styles.button }>
+          <img
+            data-testid="profile-top-btn"
+            src={ drinkIcon }
+            width="70%"
+            alt=""
+          />
+        </Link>
+        <Link to="/profile" className={ styles.button }>
+          <img
+            data-testid="profile-top-btn"
+            src={ exploreIcon }
+            width="70%"
+            alt=""
+          />
+        </Link>
+        <Link to="/profile" className={ styles.button }>
+          <img
+            data-testid="profile-top-btn"
+            src={ mealIcon }
+            width="70%"
+            alt=""
+          />
+        </Link>
+      </nav>
+    </footer>
+  );
+}
