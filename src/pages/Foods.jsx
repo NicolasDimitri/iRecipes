1422e39d2bb028d1a53c34bf42215311e0aaece9
Savio Silva <s.drksilva@gmail.com>
2022-01-30 17:47:01 -0300

    Refactor: move the searchBar component to header component and implement design base of page foods;

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ExibitionComponent from '../components/ExibitionComponent';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
-import SearchBar from '../components/SearchBar';
 import requestFoodsToAPI from '../redux/actions';
 
 export default function Foods() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(requestFoodsToAPI());
   }, [dispatch]);
+
   const data = useSelector((state) => state.requestReducers.data);
   return (
     <>
       <Header title="Foods" />
-      <SearchBar />
-      <main>
+      <main style={ { margin: '90px 0' } } className="flex flex_direction_column">
         {
           data.length > 0 && (
             data.map((item, index) => (
@@ -28,6 +29,7 @@ export default function Foods() {
           )
         }
       </main>
+      <Footer />
     </>
   );
 }
