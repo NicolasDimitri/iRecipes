18e924f694d211ccc458e5ffd1448bf702c22ede
Savio Silva <s.drksilva@gmail.com>
2022-01-30 17:44:44 -0300

    Feat: built the header component design;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,42 @@
+import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import AplicationContext from '../context/AplicationContext';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
-import AplicationContext from '../context/AplicationContext';
-import '../styles/Header.css';
+import styles from '../styles/Header.module.css';
+import SearchBar from './SearchBar';
 
 function Header({ title, renderExplore }) {
   const { toggleSearchBar } = useContext(AplicationContext);
   return (
-    <header className="header">
-      <nav className="nav">
-        <Link to="/profile">
-          <img
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt=""
-          />
-        </Link>
-        <h1 data-testid="page-title" className="title">{ title }</h1>
+    <header className={ `box primary_color ${styles.box}` }>
+      <div className="wrapper flex justify_content_between relative">
+        <nav>
+          <Link to="/profile" className={ styles.button }>
+            <img
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              width="90%"
+              alt=""
+            />
+          </Link>
+        </nav>
+        <h1 data-testid="page-title" className={ styles.title }>{ title }</h1>
         {
           renderExplore && (
-            <button type="button" className="searchBtn" onClick={ toggleSearchBar }>
+            <button type="button" className={ styles.button } onClick={ toggleSearchBar }>
               <img
                 data-testid="search-top-btn"
                 src={ searchIcon }
+                width="90%"
                 alt="search"
               />
             </button>
           )
         }
-      </nav>
+      </div>
+      <SearchBar />
     </header>
   );
 }
